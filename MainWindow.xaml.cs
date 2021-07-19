c25849dcd9efeb28a187142e87c554f88c8305ce
Lev Yastrebov <levyas@outlook.com>
2021-07-19 21:20:58 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WpfDataGridValidationRepro
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowVm();
+        }
+    }
+}
---
+++ MainWindowVm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WpfDataGridValidationRepro
+{
+    public class MainWindowVm
+    {
+        public ObservableCollection<ItemVm> Items { get; } = new()
+        {
+            new ItemVm(),
+            new ItemVm(),
+            new ItemVm(),
+        };
+    }
+
+    public class ItemVm : IDataErrorInfo
+    {
+        public string PostalCode { get; set; } = "123";
+
+        public string Error => "";
+
+        public string? this[string name] =>
+            name switch
+            {
+                nameof(PostalCode) => validatePostalCode(PostalCode),
+                _ => null,
+            };
+
+        private static string? validatePostalCode(string code)
+        {
+            return code.All(c => Char.IsDigit(c) || c == '-')
+                       ? null
+                       : "Please use digits only";
+        }
+    }
+}
